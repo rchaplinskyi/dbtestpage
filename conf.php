d2a15dd055b62f1ade29f34761a1946a0eb4b556
Roman Chaplinskyi <rchaplinskyi@gmail.com>
2023-04-10 14:49:18 +0300

    робота з підготовленими запитами в MySQL

--- conf.php
+++ conf.php
@@ -1,16 +1,25 @@
 
 <?php
 //ООП
-//    $servername = 'localhost';
-//    $username = 'admin';
-//    $password = '123456789';
-//    $dbname = 'userdb';
-//
-//    $conn = new mysqli($servername, $username, $password, $dbname );
-//    if ($conn->connect_error) {
-//        die('Зєднання з базою даних недоступне:' .$conn->connect_error);
-//    }
-//    echo 'Зєднання успішно встановлено';
+    $servername = 'localhost';
+    $username = 'admin';
+    $password = '123456789';
+    $dbname = 'userdb';
+
+    $conn = new mysqli($servername, $username, $password, $dbname );
+    if ($conn->connect_error) {
+        die('Зєднання з базою даних недоступне:' .$conn->connect_error);
+    }
+    echo 'Зєднання успішно встановлено';
+    $sql = "SELECT * FROM user";
+    $query = $conn->query($sql);
+
+    if ($query->num_rows>0) {
+        while ($row = $query->fetch_assoc()){
+            echo "<br> ID користувача".$row['id'] ."<br> Імя користувача".$row['name'] ."<br> Прізвище користувача" .$row['surname'];
+        }
+    }
+    $conn->close();
 //
 // процедурний
 //    $servername = 'localhost';
@@ -25,30 +34,46 @@
 //    echo 'Зєднання з бд успішно встановлено';
 
 //PDO
-
-    $servername = 'localhost';
-    $username = 'admin';
-    $password = '123456789';
-    $dbname = 'userdb';
-
-try {
-    $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
-    echo "Зєднання успішно встановлено";
+//
+//    $servername = 'localhost';
+//    $username = 'admin';
+//    $password = '123456789';
+//    $dbname = 'userdb';
+//
+//try {
+//    $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
+   // echo "Зєднання успішно встановлено";
 
 //    $sql = "CREATE TABLE user (id int not null auto_increment, PRIMARY KEY(id),
 //                              name varchar (15) not null,
 //                              surname varchar (20) not null,
 //                              login varchar (20) not null,
 //                              password varchar (20) not null)";
-    $sql = "INSERT INTO user (name,surname, login, password )
-                              VALUES ('Артем','Мочалов', 'amochalov','12345')";
-    $conn->exec($sql);
-    echo 'Запис успішно доданий';
-}
-  catch (PDOException $exception){
-    echo "Зєднання не встановлено".$exception->getMessage();
-    echo "Запис не доданий" .$sql.$exception->getMessage();
-  };
+//    $sql = "INSERT INTO user (name,surname, login, password )
+//
+//
+//                         VALUES ('Артем','Мочалов', 'amochalov','12345')";
+////   Створення підготовленого запиту
+//    $name="Vitaliy";
+//    $surname="Meh";
+//    $login = "vmeh";
+//    $password = '1234566';
+//    $sql = "INSERT INTO user (name,surname, login, password)
+//            VALUES(:name, :surname,:login, :pass)";
+//    $query=$conn->prepare($sql);
+//    $query->execute(['name'=> $name,'surname'=>$surname, 'login'=>$login, 'pass'=>$password]);
+//    echo 'Запис успішно доданий';
+//    $sql = "SELECT * FROM user WHERE id=1";
+//    $query = $conn->query($sql);
+//    $users = $query->fetch(PDO::FETCH_ASSOC);
+//
+//    foreach ($users as $user) {
+//        echo $user['name'].' '.$user['surname'].' '.$user['login'] .'<br>';
+//    }
+//}
+//  catch (PDOException $exception){
+//    echo "Зєднання не встановлено".$exception->getMessage();
+//  };
 
 
 
